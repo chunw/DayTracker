cce6fb7c00baa81ff7881b40f178e4d2aa5fe6d9
Chun Wang <chun@spanishdict.com>
2014-02-06 09:46:53 -0500

    Update form handler.

--- form.php
+++ form.php
@@ -43,15 +43,38 @@
               </h3>
               <p>
                   <?php
-                    $duration = 0;
                     echo "<b>Category:</b> $category <br>";
                     echo "<b>Start time:</b> $start_time <br>";
                     echo "<b>End time:</b> $end_time <br>";
-                    //TODO form field validation (check start time < end time)
-                    $start_time = substr($start_time, 0, -2);
-                    $end_time = substr($end_time, 0, -2);
-                    $hour_diff = substr($end_time,0, -3)- substr($start_time,0, -3);
-                    $minute_diff = substr($end_time, -2)- substr($start_time, -2);
+                    //TODO form field validation (check start time < end time && end time != 12:00am)
+                     $arr1 = explode(':', $start_time);
+                     $start_hour = intval($arr1[0]);
+                     $start_minute = intval($arr1[1]);
+
+                     $arr2 = explode(':', $end_time);
+                     $end_hour = intval($arr2[0]);
+                     $end_minute = intval($arr2[1]);
+
+
+                     if (substr($start_time, -2)=="am"){
+                        if ($start_hour==12) {$start_hour = 0;}
+                     } else {
+                        if ($start_hour!=12) {$start_hour = $start_hour + 12;}
+                     }
+
+                     if (substr($end_time, -2)=="am"){
+                        if ($end_hour==12) {$end_hour = 0;}
+                     } else {
+                        if ($end_hour!=12) {$end_hour = $end_hour + 12;}
+                     }
+
+                    $hour_diff = $end_hour - $start_hour;
+                    $minute_diff = $end_minute - $start_minute ;
+
+                    if ($minute_diff < 0) {
+                      $hour_diff = $hour_diff -1;
+                      $minute_diff = 60 + $minute_diff;
+                    }
                     echo "<b>Duration:</b> $hour_diff hours, $minute_diff mintues <br>";
                     echo "<b>Notes:</b> $notes <br>";
                     echo "<b>Media link:</b> $media_link <br>";
